816a92eba1c49e9170583df61041e48fedb3108c
QingYuWeng <30350111+QingYuWeng@users.noreply.github.com>
2017-10-12 11:42:07 +0800

    chat

---
+++ app/src/main/java/com/example/yuwenqing/system_enterprise/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuwenqing.system_enterprise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
